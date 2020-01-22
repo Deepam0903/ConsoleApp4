d43a0206b2d7847f08f0eb3856a4ffb348028c10
deepa <deepa@DESKTOP-NKH64E3>
2020-01-21 20:58:00 -0500

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+            //Max heart Rate//
+            int heart_rate;
+         
+            age = 26;
+            height = 153;
+            heart_rate = 220 - age;
+            Console.WriteLine("I am " + age + " old, my max heart rate is " + heart_rate + " and I am " + height + " cms tall.");
+
+
+        }
+    }
+}
